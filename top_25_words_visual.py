eda587022f7b98d28513165e2736ab7e2d3a632b
matt-l-1 <83447667+matt-l-1@users.noreply.github.com>
2024-04-24 15:55:33 +0100

    Update top_25_words_visual.py

--- top_25_words_visual.py
+++ top_25_words_visual.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 df = pd.read_excel("top_100_words_in_politics.xlsx")
 
+plt.style.use("ggplot")
+plt.autoscale()
 plt.barh(df["Word"][:25], df["SUM"][:25])
 plt.ylabel("Word")
 plt.xlabel("Word Frequency")
